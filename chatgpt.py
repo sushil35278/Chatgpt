3c28ac38e256f807556b49b1f2fa60de3a796dc5
sushil sigdel <62206321+sushil35278@users.noreply.github.com>
2023-02-23 11:16:38 +0545

    new commit

---
+++ chatgpt.py
@@ -0,0 +1,16 @@
+import openai
+
+openai.api_key = "your api"
+model_engine = "text-davinci-003"
+prompt = str(input())
+
+completion = openai.Completion.create(
+	engine=model_engine,
+	prompt=prompt,
+	max_tokens=1024,
+	n=1,
+	stop=None,
+	temperature=0.5,
+)
+response = completion.choices[0].text
+print(response)
